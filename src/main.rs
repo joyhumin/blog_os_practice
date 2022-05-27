c853f4b2f7fbbfd1b447d7a18930175ab5dc2275
Joy HU <joyhumin@gmail.com>
2022-05-27 21:02:51 +1200

    feat: set up test framework

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 // disable standard library links
 #![no_std]
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
@@ -14,7 +17,8 @@ pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+    #[cfg(test)]
+    test_main();
     loop {
     }
 }
@@ -28,3 +32,18 @@ fn panic(info: &PanicInfo) -> ! {
 
 // linker, combines the generated code into an executable.
 
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test()
+    }
+}
+
+#[test_case]
+fn trivial_assertion(){
+    print!("trivial assertion ...");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
